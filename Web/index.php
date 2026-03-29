a7a0ecfa6e0af538151cbdeb3efb8030754d029a
chau11ece <chau11ece@yahoo.com>
2026-03-29 17:39:24 +0700

    update: skip test stage CI/CD pipeline

--- Web/index.php
+++ Web/index.php
@@ -36,7 +36,6 @@
     ob_end_flush();
 ?>
 <!-- CI/CD Test Deploy - 2026-03-29 05:05 UTC - SSH key fix applied -->
-<!-- CI/CD Test Deploy - 2026-03-29 05:15 UTC - SSH key updated -->
 <!-- CI/CD Test Deploy - 2026-03-29 09:14 UTC - Database service added to test job -->
 <!-- CI/CD Test Deploy - chmod -R 777 /var/www/html/application/logs -->
 <!-- CI/CD Test Deploy - docker build --no-cache --pull -t mu-web:latest -->
