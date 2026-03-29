c63345b701527bbbd782543ad2aaeddfd86ed5bc
chau11ece <chau11ece@yahoo.com>
2026-03-29 10:34:47 +0700

    test: trigger CI/CD pipeline after ansible-mu checkout fix

--- Web/index.php
+++ Web/index.php
@@ -35,4 +35,4 @@
 
     ob_end_flush();
 ?>
-<!-- CI/CD Test Deploy - 2026-03-29 03:20 UTC - DockerHub Secrets Added -->
+<!-- CI/CD Test Deploy - 2026-03-29 03:34 UTC - GHCR_TOKEN for ansible-mu checkout -->
